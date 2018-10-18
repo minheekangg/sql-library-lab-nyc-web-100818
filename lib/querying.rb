ed9a2d770f1e57b9e9e6e0ac467995eaeea878ae
Min Hee  Park <minheekangg@gmail.com>
2018-10-18 20:04:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+FROM characters
+GROUP BY characters.species
+ORDER BY count(characters.species) DESC limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
