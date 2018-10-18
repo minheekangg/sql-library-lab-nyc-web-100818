ff06a4febd274dbea612ebfd190f7c30ad5aa487
Min Hee  Park <minheekangg@gmail.com>
2018-10-18 19:35:31 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,14 @@
-CREATE TABLE Characters (
+CREATE TABLE Series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id TEXT,
   sub-genre_id TEXT
-)
+);
+
+CREATE TABLE SubGenres (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id TEXT,
+  sub-genre_id TEXT
+);
+
