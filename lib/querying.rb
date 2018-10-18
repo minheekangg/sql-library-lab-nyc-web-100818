f0494f9b134107877350e149aebc29630ae85564
Min Hee  Park <minheekangg@gmail.com>
2018-10-18 19:54:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   JOIN series
   ON books.series_id = series_id
   ORDER BY books.year
-  WHERE series_id = "1";"
+  WHERE series_id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
